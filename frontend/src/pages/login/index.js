e2f1d4abf56b5c8df868e9caa2cb2d21923a4025
Bruno Souza <bruno.arubesu@gmail.com>
2020-03-28 15:31:57 -0300

    add login api integration

--- frontend/src/pages/login/index.js
+++ frontend/src/pages/login/index.js
@@ -1,24 +1,47 @@
-import React from 'react';
-import { Link } from 'react-router-dom'
+import React, { useState } from 'react';
+import { Link, useHistory } from 'react-router-dom'
 import { FiLogIn } from 'react-icons/fi';
 
 import './styles.css';
 import heroesImg from '../../assets/heroes.png';
 import logoImg from '../../assets/logo.svg';
 
+import api from '../../services/api';
+
 export default function Login() {
+
+  const [id, setId] = useState('');
+  const history = useHistory();
+
+  async function handleLogin(e) {
+    e.preventDefault();
+
+    api.post('sessions', { id })
+      .then(response => {
+        localStorage.setItem('ngoId', id);
+        localStorage.setItem('ngoName', response.data.name);
+        history.push('/profile');
+      })
+      .catch(err => alert(err));
+  }
+
   return (
     <div className="logon-container">
       <section className="form">
         <img src={logoImg} alt="Be The Hero" />
-        <form >
+        <form onSubmit={handleLogin} >
           <h1>Faça seu logon</h1>
-          <input placeholder="Sua ID " />
+
+          <input
+            placeholder="Sua ID "
+            onChange={e => setId(e.target.value)}
+          />
           <button className="button" type="submit">Entrar</button>
 
           <Link className="back-link" to="/register">
             <FiLogIn size={16} color="#e02041" />
-            Não tenho cadastro</Link>
+            Não tenho cadastro
+          </Link>
         </form>
       </section>
       <img src={heroesImg} alt="Heroes" />
