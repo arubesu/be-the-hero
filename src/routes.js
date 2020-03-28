ac7ebb2c4792584134ed4ffe975e49ffc0b47da0
Bruno Souza <bruno.arubesu@gmail.com>
2020-03-27 23:38:06 -0300

    add session and profile controllers

--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,8 @@ const routes = express.Router();
 
 const ngoController = require('./app/controllers/ngoController');
 const incidentController = require('./app/controllers/incidentController');
+const profileController = require('./app/controllers/profileController');
+const sessionController = require('./app/controllers/sessionController');
 
 /**
  * List All NGOs
@@ -13,6 +15,12 @@ routes.get('/ngos', ngoController.index);
  * create NGO
  */
 routes.post('/ngos', ngoController.store);
+
+/**
+ * PROFILE ROUTES
+ */
+
+routes.get('/profile', profileController.index);
 
 /**
  * INCIDENTS ROUTES
@@ -32,5 +40,16 @@ routes.post('/incidents', incidentController.store);
  * DELETE Incident
  */
 routes.delete('/incidents/:id', incidentController.delete);
+
+
+/**
+ * SESSION
+ */
+
+
+/**
+ * CREATE SESSION
+ */
+routes.post('/sessions', sessionController.create);
 
 module.exports = routes;
