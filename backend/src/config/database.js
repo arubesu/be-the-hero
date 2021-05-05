d6b1af3a7deef3208d782a52c7e4b77e760a3c9d
Bruno Souza <bruno.arubesu@gmail.com>
2021-05-04 21:33:30 -0300

    change database

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -2,7 +2,7 @@ require('dotenv').config()
 
 module.exports = {
   dialect: 'postgres',
-  database: 'be-the-hero',
+  database: 'postgres',
   host: process.env.DB_HOST,
   username: process.env.DB_USER,
   password: process.env.DB_PASS,
