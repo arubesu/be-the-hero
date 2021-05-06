d9f2f091e59acef93a8e1244a26ed3c856ffa199
Bruno Souza <bruno.arubesu@gmail.com>
2021-05-05 22:56:44 -0300

    translate and fix ngo data

--- mobile/src/pages/detail/index.js
+++ mobile/src/pages/detail/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, TouchableOpacity, Text, Image, Linking } from 'react-native';
+import { View, TouchableOpacity, Text, Image, Linking, SafeAreaView, ScrollView } from 'react-native';
 import { useNavigation, useRoute } from '@react-navigation/native';
 import { Feather } from '@expo/vector-icons';
 import * as MailComposer from 'expo-mail-composer';
@@ -12,17 +12,18 @@ export default function Detail() {
   const route = useRoute();
 
   const { incident } = route.params;
+  const { Ngo: ngo } = incident;
 
-  const message = `Olá ${incident.name}, estou entrando em contato pois gostaria de ajudar no
-  caso "${incident.title}" com o valor de ${Intl.NumberFormat('pt-BR', {
+  const message = `Hi ${ngo.name}, I'm getting in touch because I would like to help
+  in the cause "${incident.title}" with the value of ${Intl.NumberFormat('en-US', {
     style: 'currency',
-    currency: 'BRL'
+    currency: 'USD'
   }).format(incident.value)}`;
 
   function sendMail() {
     MailComposer.composeAsync({
       subject: incident.title,
-      recipients: [incident.email],
+      recipients: [ngo.email],
       body: message
     });
   }
@@ -32,8 +33,8 @@ export default function Detail() {
   }
 
   return (
-    <View style={styles.container}>
 
+    <SafeAreaView style={styles.container}>
       <View style={styles.header}>
         <Image source={logoImg} />
         <TouchableOpacity onPress={navigator.goBack}>
@@ -41,40 +42,44 @@ export default function Detail() {
         </TouchableOpacity>
       </View>
 
-      <View style={styles.incident}>
-        <Text style={styles.incidentProperty}>ONG:</Text>
-        <Text style={styles.incidentValue}>{incident.name} de {incident.city}/{incident.uf}</Text>
+      <ScrollView
+        showsVerticalScrollIndicator={false}
+        showsHorizontalScrollIndicator={false}>
+        <View style={styles.incident}>
+          <Text style={styles.incidentProperty}>NGO:</Text>
+          <Text style={styles.incidentValue}>{ngo.name} from {ngo.city}/{ngo.state}</Text>
 
-        <Text style={styles.incidentProperty}>CASO:</Text>
-        <Text style={styles.incidentValue}>{incident.description}</Text>
+          <Text style={styles.incidentProperty}>CAUSE:</Text>
+          <Text style={styles.incidentValue}>{incident.description}</Text>
 
-        <Text style={styles.incidentProperty}>VALOR:</Text>
-        <Text style={styles.incidentValue}>{Intl.NumberFormat('pt-BR', {
-          style: 'currency',
-          currency: 'BRL'
-        }).format(incident.value)}
-        </Text>
-      </View>
-
-      <View style={styles.contactBox} >
-        <Text style={styles.contactBoxTitle} >Salve o dia!</Text>
-        <Text style={styles.contactBoxTitle} >Seja o herói desse caso</Text>
-
-        <Text style={styles.contactBoxDescription} >Entre em contato:</Text>
-        <View style={styles.actions}>
-          <TouchableOpacity
-            style={styles.action}
-            onPress={sendWhatsApp}>
-            <Text style={styles.actionText}>WhatsApp</Text>
-          </TouchableOpacity>
-
-          <TouchableOpacity
-            style={styles.action}
-            onPress={sendMail}>
-            <Text style={styles.actionText}>E-mail</Text>
-          </TouchableOpacity>
+          <Text style={styles.incidentProperty}>VALUE:</Text>
+          <Text style={styles.incidentValue}>{Intl.NumberFormat('pt-BR', {
+            style: 'currency',
+            currency: 'USD'
+          }).format(incident.value)}
+          </Text>
         </View>
-      </View>
-    </View>
+
+        <View style={styles.contactBox} >
+          <Text style={styles.contactBoxTitle} >Save the day!</Text>
+          <Text style={styles.contactBoxTitle} >Be the hero of this cause</Text>
+
+          <Text style={styles.contactBoxDescription} >Contact:</Text>
+          <View style={styles.actions}>
+            <TouchableOpacity
+              style={styles.action}
+              onPress={sendWhatsApp}>
+              <Text style={styles.actionText}>WhatsApp</Text>
+            </TouchableOpacity>
+
+            <TouchableOpacity
+              style={styles.action}
+              onPress={sendMail}>
+              <Text style={styles.actionText}>E-mail</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   )
 }
--- mobile/src/pages/incidents/index.js
+++ mobile/src/pages/incidents/index.js
@@ -47,11 +47,11 @@ export default function Incidents() {
     <View style={styles.container}>
       <View style={styles.header}>
         <Image source={logoImg} />
-        <Text style={styles.headerText}>Total de <Text style={styles.headerTextBold}>{total} casos</Text>.</Text>
+        <Text style={styles.headerText}>Total of <Text style={styles.headerTextBold}>{total} causes</Text>.</Text>
       </View>
 
-      <Text style={styles.title}>Bem vindo</Text>
-      <Text style={styles.description}>Escolha um dos casos abaixo e salve o dia</Text>
+      <Text style={styles.title}>Welcome</Text>
+      <Text style={styles.description}>Choose one of the cases below and save the day</Text>
 
       <FlatList
         data={incidents}
@@ -63,16 +63,16 @@ export default function Incidents() {
         renderItem={({ item: incident }) => (
 
           <View style={styles.incident}>
-            <Text style={styles.incidentProperty}>ONG:</Text>
-            <Text style={styles.incidentValue}>{incident.name}</Text>
+            <Text style={styles.incidentProperty}>NGO:</Text>
+            <Text style={styles.incidentValue}>{incident.Ngo.name}</Text>
 
-            <Text style={styles.incidentProperty}>CASO:</Text>
+            <Text style={styles.incidentProperty}>CAUSE:</Text>
             <Text style={styles.incidentValue}>{incident.description}</Text>
 
-            <Text style={styles.incidentProperty}>VALOR:</Text>
-            <Text style={styles.incidentValue}>{Intl.NumberFormat('pt-BR', {
+            <Text style={styles.incidentProperty}>VALUE:</Text>
+            <Text style={styles.incidentValue}>{Intl.NumberFormat('en-US', {
               style: 'currency',
-              currency: 'BRL'
+              currency: 'USD'
             }).format(incident.value)}
             </Text>
 
@@ -80,7 +80,7 @@ export default function Incidents() {
               style={styles.detailsButton}
               onPress={() => navigateToDetail(incident)}
             >
-              <Text style={styles.detailsButtonText}>Ver mais detalhes</Text>
+              <Text style={styles.detailsButtonText}>View more details</Text>
               <Feather name='arrow-right' size={16} color='#e02041' ></Feather>
             </TouchableOpacity>
           </View>
